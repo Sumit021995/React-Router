9abe29da029b3f537b79072dbae76f5c6fcc33d2
Sumit Saurav <107242767+Sumit021995@users.noreply.github.com>
2024-04-21 13:42:43 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -67,8 +67,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a href="https://sumitsauravportfolio.netlify.app/" className="hover:underline">
+                            sumitsaurav
                         </a>
                         . All Rights Reserved.
                     </span>
